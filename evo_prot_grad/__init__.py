1c135b8216708b18970f823e378f0bf665434851
Patrick Emami <pemami@nrel.gov>
2023-07-17 18:30:40 -0500

    typo in get_expert doc

--- evo_prot_grad/__init__.py
+++ evo_prot_grad/__init__.py
@@ -14,6 +14,7 @@ def get_expert(expert_name: str,
                use_without_wildtype: bool = False) -> Expert:
     """
     Current supported expert types (to pass to argument `expert_name`):
+    
         - `bert`
         - `causallm`
         - `esm`
