92d989e629f6cb49d9b10cf5cdfe2911b0829453
Patrick Emami <pemami@nrel.gov>
2023-09-07 10:29:16 -0500

    Moving try/except to improve error messaging

--- evo_prot_grad/__init__.py
+++ evo_prot_grad/__init__.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 from transformers import PreTrainedTokenizerBase
 from evo_prot_grad.experts.base_experts import Expert
 from evo_prot_grad.common.tokenizers import ExpertTokenizer
-from evo_prot_grad.common.sampler import DirectedEvolution
 
 def get_expert(expert_name: str,
                temperature: float = 1.0,               
@@ -51,12 +50,14 @@ def get_expert(expert_name: str,
     """
     try:
         expert_mod = importlib.import_module(f"evo_prot_grad.experts.{expert_name}_expert")
-        return expert_mod.build(
-            temperature = temperature,
-            model = model,
-            tokenizer = tokenizer,
-            device = device,
-            use_without_wildtype = use_without_wildtype
-        )
     except:
         raise ValueError(f"Expert {expert_name} not found in evo_prot_grad.experts.")
+            
+    return expert_mod.build(
+        temperature = temperature,
+        model = model,
+        tokenizer = tokenizer,
+        device = device,
+        use_without_wildtype = use_without_wildtype
+    )
+    
